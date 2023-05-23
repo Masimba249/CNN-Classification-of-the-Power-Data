05ee5b49ffb3ad636e04520ac02d1e9fad6ac167
Collins Masimba <130909453+Masimba249@users.noreply.github.com>
2023-05-23 13:13:28 +0600

    Add files via upload

---
+++ lab_09_wc.m
@@ -0,0 +1,11 @@
+clear all;
+close all; clc;
+x=line([2,4],[4,4],'lineWidth',2);
+
+line([2,4],[2,2],'lineWidth',2);
+line([1,2],[3,4],'lineWidth',2);
+line([5,4],[3,4],'lineWidth',2);
+line([5,4],[3,2],'lineWidth',2);
+line([1,2],[3,2],'lineWidth',2);
+axis([0 6 0 6])
+grid on
